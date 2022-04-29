36b075991da55e7970b865f93653432cecd937cf
grandevx <vittoriodalfonso07@gmail.com>
2022-04-29 18:24:08 +0200

    static website base

---
+++ website/Static/synapse_show/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+
+      colors : {
+        yellow : {
+          '600' : '#c0df34',
+        }
+      }
+
+    },
+  },
+  plugins: [],
+}
