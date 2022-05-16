edcec55ccc9c9ba26dd4fc15ad7e599028fd1813
grandevx <vittoriodalfonso07@gmail.com>
2022-05-17 01:14:22 +0200

    Register / Login API Implementation

--- API/platform_api/server.js
+++ API/platform_api/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const mysql = require("mysql");
- 
+require('dotenv').config();
+
 const app = express();
  
 app.use(cors());
@@ -11,10 +12,10 @@ app.use(bodyParser.json());
   
 //create database connection4
 const conn = mysql.createConnection({
-  host: '162.19.3.245',
-  user: 'apiuser',
-  password: 'ZhbfpwCBbiYNgX4pIteI',
-  database: 'synapse'
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
  
 //connect to database
@@ -34,18 +35,41 @@ app.post('/login', (req, res) => {
     let query = conn.query(sql, (err, results) => {
         if(err) throw err;
         if(results.length > 0){
-            res.send(results);
+            console.log(results);
+            res.send({code: 200, message: 'Login Successful', data: {identifier: results[0].username}});
         }else{
-            res.send('0');
+            res.send({code: 404, message: 'Login Failed'});
         }
     });
-
     console.log(query.sql);
 
 }); 
 
+app.post('/register', (req, res) => {
 
-//add new user
+    let email = req.body.email;
+    let password = req.body.password;
+    let username = req.body.username;
+
+
+    let sql = `SELECT * FROM users WHERE username = '${username}'`;
+
+    let query = conn.query(sql, (err, results) => {
+        if(err) throw err;
+        if(results.length > 0){
+            res.send({code: 100, message: 'Username già esistente'});  
+        }else{
+          let sql = `INSERT INTO users (email, password, username) VALUES ('${email}', '${password}', '${username}')`;
+          let query = conn.query(sql, (err, results) => {
+              if(err) throw err;
+              res.send({code: 200, message: 'Registrazione Avvenuta'});
+          });
+        }
+
+    });
+
+});
+
 app.post('/store-data',(req, res) => {
   let data = {name: req.body.name};
   let sql = "INSERT INTO users SET ?";
@@ -55,6 +79,6 @@ app.post('/store-data',(req, res) => {
   });
 });
  
-app.listen(3100, () => {
+app.listen(3002, () => {
   console.log("Server running successfully on 3000");
 });
