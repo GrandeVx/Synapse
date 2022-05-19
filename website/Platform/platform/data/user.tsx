39034a1dcf3ed5de742a8ede4e10c5b1bf48b8cf
grandevx <vittoriodalfonso07@gmail.com>
2022-05-19 03:25:22 +0200

    Sensors Fetch/Add Logic + Notification System

--- website/Platform/platform/data/user.tsx
+++ website/Platform/platform/data/user.tsx
@@ -4,6 +4,8 @@ import Axios from 'axios';
 import { getDatabase, ref, onValue} from "firebase/database";
 import inizializeDatabase from "./initFirebase";
 
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 interface sensore {
   id: string,
@@ -41,6 +43,62 @@ const UserStore = create<{
 }));
 
 
+export const getSensorData = async (data : any ,store : any) => {
+
+  data.map((sensor) => {
+
+    if (sensor != null) {
+      Axios.post('http://localhost:3002/getSensorData', {
+        sensor_id: sensor.id
+      }).then(function (response) {
+        if (response.data.code === 200) {
+          sensor.data = response.data.data;
+          store.setSensor(data);
+        }  
+      }).catch(function (error) {
+        console.log(error);
+      }
+      );
+    }});
+    
+  
+}
+
+
+export const addSensor = async (id:string,nome:string,descrizione:string,altezza:string,username:string , Data : any, SetOpen: any) => {
+
+  await Axios.post('http://localhost:3002/addSensor', {
+    username:username,
+    id: id,
+    nome: nome,
+    descrizione: descrizione,
+    altezza: altezza
+  }).then((response) => {
+
+    if (response.data.code === 101) {
+      throw "Sensore Già Registrato";
+    }
+
+    else if (response.data.code === 200) {
+      let data = {
+        id: id,
+        data: [],
+        measure: 0,
+        name : nome,
+        description: descrizione,
+        timestamp: "0-0-0"
+      }
+      let result = Data.sensors;
+      result.push(data);
+      getSensor(username,Data);
+      Notify.success("Sensore Aggiunto Con Successo");
+      SetOpen(false);
+    } else throw "Sensore Non Esistente";
+  }).catch((err) =>  Notify.failure(err));;
+
+
+}
+
 
 const getSensor = async (id: string,store : any) => {
   
@@ -48,29 +106,51 @@ const getSensor = async (id: string,store : any) => {
     username: id
   }).then((response) => {
     if (response.data.code === 200) {
+
+      console.log(response.data);
+
       let result = [];
       response.data.data.sensors.map((sensor) => {
         result.push({
           id: sensor.sensor_id,
+          data: [],
           measure: 12,
+          name : sensor.name,
+          description: sensor.description,
           timestamp: "0-0-0"
         });
+
         const db = getDatabase();
         result.map((sensor) => {
-          console.log(sensor);
           const starCountRef = ref(db, sensor.id);
           onValue(starCountRef, (snapshot) => {
-            const data = snapshot.val();
-            sensor.measure = data.measure;
-            sensor.timestamp = data.timestamp;
-            store.setSensor(result);
+            const datarec = snapshot.val();
+            let new_data;
+            
+            //@ts-ignore
+            if (sensor.data.sensor_data) new_data = {sensor_data:sensor.data.sensor_data};
+            else new_data = {sensor_data:[]};
+
+            if (datarec) {
+              new_data.sensor_data.push({sensor_id : sensor.id, data : Number(datarec.measure), timestamp: String(datarec.timestamp)});
+              console.log(new_data);
+              sensor.measure = datarec.measure;
+              sensor.timestamp = datarec.timestamp;
+              sensor.data = new_data ;
+              console.log(sensor.data);
+              store.setSensor(result);
+            }
           });
 
         });
 
       })
+
+      getSensorData(result,store);
+
+
     } else return []
-  }).catch((err) => console.log("Errore", err));;
+  }).catch((err) =>  Notify.failure(err))
 
 
 
@@ -88,13 +168,15 @@ export const LoginUser = async (email: string, password: string, store : any) =>
       let identifier = response.data.data.identifier;
       getSensor(identifier,store);
       store.login(identifier);
+      Notify.success("Login Effettuato Con Successo");
     }
-  }).catch((err) => console.log("Errore", err));;
+    else throw "Email o Password Errati";
+  }).catch((err) => Notify.failure(err));;
 
 }
 
 
-export const RegisterUser = async (username:string,email: string, password: string) => {
+export const RegisterUser = async (username:string,email: string, password: string,setRegister:any) => {
 
   await Axios.post('http://localhost:3002/register', {
     email: email,
@@ -102,12 +184,14 @@ export const RegisterUser = async (username:string,email: string, password: stri
     username: username
   }).then((response) => {
     // se tutto va bene ci pensa il .then nel Register.tsx || per il resto controlliamo gli errori per le notifiche
-    if (response.data.code === 100) throw new Error("Email già in uso");
-  }).catch((err) => console.log("Errore", err));;
+    if (response.data.code === 100) throw "Username o Email già in uso";
+    else if (response.data.code === 200) {
+      setRegister(false)
+      Notify.success("Registrazione avvenuta con successo");
+    }
+  }).catch((err) => Notify.failure(err));;
 
 }
 
-
-  
 
 export default UserStore;
