fb0f76d9a2afa6e75ad3b0fb9235f4bdae35cd26
grandevx <vittoriodalfonso07@gmail.com>
2022-05-19 03:24:12 +0200

    Sensor Fetch/Add System + Register/Login

--- API/platform_api/server.js
+++ API/platform_api/server.js
@@ -28,20 +28,21 @@ app.post('/login', (req, res) => {
     
     let data = req.body.email;
     let data2 = req.body.password;
-    
-    console.log(data)
 
     let sql = `SELECT * FROM users WHERE email = '${data}' AND password = '${data2}'`;
+
     let query = conn.query(sql, (err, results) => {
         if(err) throw err;
+
         if(results.length > 0){
-            console.log(results);
-            res.send({code: 200, message: 'Login Successful', data: {identifier: results[0].username}});
-        }else{
-            res.send({code: 404, message: 'Login Failed'});
+            return res.send({code: 200, message: 'Login Successful', data: {identifier: results[0].username}});
+        }
+        else{
+            return res.send({code: 404, message: 'Login Failed'});
         }
     });
-    console.log(query.sql);
+
+
 
 }); 
 
@@ -52,33 +53,110 @@ app.post('/register', (req, res) => {
     let username = req.body.username;
 
 
-    let sql = `SELECT * FROM users WHERE username = '${username}'`;
+    let sql = `SELECT * FROM users WHERE username = '${username}' OR email = '${email}'`;
 
-    let query = conn.query(sql, (err, results) => {
+        conn.query(sql, (err, results) => {
         if(err) throw err;
         if(results.length > 0){
-            res.send({code: 100, message: 'Username già esistente'});  
+            return res.send({code: 100, message: 'Username già esistente'});  
         }else{
           let sql = `INSERT INTO users (email, password, username) VALUES ('${email}', '${password}', '${username}')`;
           let query = conn.query(sql, (err, results) => {
               if(err) throw err;
-              res.send({code: 200, message: 'Registrazione Avvenuta'});
+              return res.send({code: 200, message: 'Registrazione Avvenuta'});
           });
         }
 
     });
 
+    return;
+
 });
 
-app.post('/store-data',(req, res) => {
-  let data = {name: req.body.name};
-  let sql = "INSERT INTO users SET ?";
-  let query = conn.query(sql, data,(err, results) => {
-    if(err) throw err;
-    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+
+app.post('/getSensor', (req, res) => {
+    
+  let data = req.body.username;
+
+  console.log(data)
+
+  let sql = `SELECT sensor_id , name , description , location FROM sensors_available WHERE username = '${data}'`;
+  conn.query(sql, (err, sensors) => {
+      if(err) throw err;
+      if(sensors.length > 0){
+        let tmp = []
+        sensors.map((sensor, index) => {
+        
+            let sql = `SELECT * FROM sensors_data WHERE sensor_id = '${sensor.sensor_id}'`;
+            conn.query(sql, (err, sensors_data) => {
+                if(err) throw err;
+                if(sensors_data.length > 0){
+                    tmp.push(sensors_data)
+                }
+            });       
+        });
+        return res.send({code: 200, message: 'Sensori Trovati', data: {sensors : sensors, sensors_datas: tmp }});
+
+      }else{
+          return res.send({code: 404, message: 'Nessun Sensore Trovato'});
+      }
   });
+
+  return;
+
+}); 
+
+app.post('/getSensorData', (req, res) => {
+    let sensor_id = req.body.sensor_id;
+
+    let sql = `SELECT * FROM sensors_data WHERE sensor_id = '${sensor_id}'`;
+    conn.query(sql, (err, results) => {
+        if(err) throw err;
+        if(results.length > 0){
+            return res.send({code: 200, message: 'Dati Sensore Trovati', data: {sensor_data : results}});
+        }else{
+           return res.send({code: 404, message: 'Nessun Dato Trovato'});
+        }
+    });
 });
  
+
+app.post('/addSensor', (req, res) => {
+
+    let user = req.body.username;
+    let id = req.body.id;
+    let nome = req.body.nome;
+    let descrizione = req.body.descrizione;
+    let altezza = req.body.altezza;
+
+
+    let sql = `SELECT sensor_id , username FROM sensors_available WHERE sensor_id = '${id}'`;
+
+    conn.query(sql, (err, results) => {
+        if(err) throw err;
+        if(results.length > 0){
+            if (results[0].username == '') {
+            
+                let sql = `UPDATE sensors_available SET username = '${user}' ,name = '${nome}' ,description = '${descrizione}' ,alarm_height='${altezza}' WHERE sensor_id = '${id}'`;
+                conn.query(sql, (err, results) => {
+                    if(err) throw err;
+                    return res.send({code: 200, message: 'Aggiornamento Avvenuto'});
+                    return;
+                });
+
+            } 
+            
+        }else{
+            return res.send({code: 100, message: 'Sensore Non Registrato sulla Piattaforma'});  
+        }
+    });
+
+
+
+})
+
+
+
 app.listen(3002, () => {
   console.log("Server running successfully on 3000");
 });
